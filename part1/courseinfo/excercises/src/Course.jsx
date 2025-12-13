74e67d7dab3245fb50d1adcc57444abca5e5e9e9
Jason <jasonlimnzz@gmail.com>
2025-12-13 19:52:29 +1300

    excercise 2.1

---
+++ part1/courseinfo/excercises/src/Course.jsx
@@ -0,0 +1,17 @@
+const Course = (props) => {
+    const {course} = props
+
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            {course.parts.map(part =>
+                <p key={part.id}>
+                    {part.name} {part.exercises}
+                </p>
+            )}
+        </div>
+        
+    )
+}
+
+export default Course
