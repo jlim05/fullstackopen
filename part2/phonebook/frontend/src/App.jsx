4aff022c2581a5a69287f371a2104d422a265dd0
Jason <jasonlimnzz@gmail.com>
2026-01-22 18:15:00 +1300

    exercise 2.10

--- part2/phonebook/frontend/src/App.jsx
+++ part2/phonebook/frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { use } from 'react'
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -13,7 +16,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
@@ -57,24 +60,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with {''}
-        <input value={filter} onChange={handleFilterChange}/>
-      </div>
+      <Filter filter={filter} setFilter={setFilter}/>
 
       <h2>add a new</h2>
 
-      <form onSubmit={addName}>
-        <div>name: <input value={newName} onChange={handleNameChange}/></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+
       <h2>Numbers</h2>
-        {personToShow.map((person, index) => (
-          <div key={index}>{person.name} {person.number}</div>
-        ))}
+
+      <Persons persons={personToShow}/>
+      
     </div>
   )
 }
