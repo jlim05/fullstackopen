7ae4706a531c4e3b1e070e1fb61f6e5aedceccf9
Jason <jasonlimnzz@gmail.com>
2026-01-22 20:14:02 +1300

    exercise 2.11

--- part2/phonebook/frontend/src/App.jsx
+++ part2/phonebook/frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { use } from 'react'
-import { useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() =>{
+    fetch('http://localhost:3001/persons')
+    .then(response => response.json())
+    .then(data => setPersons(data))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,6 +35,7 @@ const App = () => {
     
       setPersons(persons.concat(personObject))
       setNewName('') 
+      setNewNumber('')
   }
 
 
