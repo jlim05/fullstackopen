c2654bbba2eebc948ccf2087b6f4d630b3380e4c
Jason <jasonlimnzz@gmail.com>
2026-01-27 00:46:19 +1300

    exercise 2.16

--- part2/phonebook/frontend/src/App.jsx
+++ part2/phonebook/frontend/src/App.jsx
@@ -3,12 +3,15 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
+
 
   useEffect(() => {
     personService
@@ -53,6 +56,7 @@ const App = () => {
         .create(newPerson)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+          setNotification(`Added ${returnedPerson.name}`)
           setNewName('')
           setNewNumber('')
         })
@@ -78,6 +82,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification}/>
       <Filter filter={filter} setFilter={setFilter} />
 
       <h2>add a new</h2>
--- part2/phonebook/frontend/src/services/persons.js
+++ part2/phonebook/frontend/src/services/persons.js
@@ -1,26 +1,23 @@
 import axios from "axios";
-
 const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  return axios.get(baseUrl)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-const create = (newObject) => {
-  return axios.post(baseUrl, newObject)
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
-const update = (id, personObject) => {
-  return axios.put(`${baseUrl}/${id}`, personObject)
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
-const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`)
-}
-
-export default {
-  getAll,
-  create,
-  update,
-  remove
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update 
 }
