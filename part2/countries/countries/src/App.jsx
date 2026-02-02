99aabdd775af4eb310e9317fd3faaac0b6ae9c83
Jason <jasonlimnzz@gmail.com>
2026-02-02 23:06:50 +1300

    exercise 2.20

--- part2/countries/countries/src/App.jsx
+++ part2/countries/countries/src/App.jsx
@@ -6,6 +6,7 @@ import "./App.css"
 const App = (props) => {
  const [countries, setCountries] = useState([])
  const [search, setSearch] = useState('')
+ const [weather, setWeather] = useState(null)
 
  const filteredCountries = countries.filter(country => 
   country.name.common
@@ -20,6 +21,20 @@ const App = (props) => {
       setCountries(response.data)
     })
  }, [])
+
+ useEffect(() => {
+    if (filteredCountries.length === 1) {
+    const capital = filteredCountries[0].capital?.[0]
+    const apiKey = import.meta.env.VITE_WEATHER_KEY
+
+    if (capital) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`)
+      .then(response => {
+        setWeather(response.data)
+      })
+    }
+  }
+ }, [filteredCountries])
 
  return (
   <div>
@@ -44,6 +59,20 @@ const App = (props) => {
             </ul>
           </div>
           <img src={filteredCountries[0].flags.svg} alt='flag' width={100}/>
+          <div>
+            <h2>Weather in {filteredCountries[0].capital?.[0]}</h2>
+
+            {weather && (
+              <div>
+                <p>Temperature {weather.main.temp} Celsius</p>
+                <img
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="weather icon"
+                />
+                <p>Wind: {weather.wind.speed} m/s</p>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <div>
