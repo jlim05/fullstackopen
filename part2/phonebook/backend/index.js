67f41dd3565d4dda1037b0cf23a561592c8e7f02
Jason <jasonlimnzz@gmail.com>
2026-02-12 14:39:22 +1300

    exercise 3.2

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -31,6 +31,14 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+
+app.get('/api/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length}</p>
+        <p>${new Date()}</p>`
+    )
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
