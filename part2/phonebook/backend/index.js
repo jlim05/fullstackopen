4d7c96985d846b79e63fae8986cc7a724243d262
Jason <jasonlimnzz@gmail.com>
2026-02-17 21:41:54 +1300

    exercise 3.11

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -10,6 +10,8 @@ morgan.token('body', (req) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+
 
 
 const generateId = () => {
